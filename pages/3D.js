863b1252ed5373f716a7d7eeaa668febf99c7666
Ayato-san <ayatosan.pro@gmail.com>
2022-02-09 18:11:29 +0100

    Ajout de la page 3D au site

--- pages/3D.js
+++ pages/3D.js
@@ -1,12 +1,33 @@
-import { Heading } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
+import Image from 'next/image'
+import { ImgGrid } from '../components/grid-item'
 
 const CGI = () => (
     <Layout title="3D">
-        <Heading as="h3" variant="section-title">
-            Models 3D
-        </Heading>
-        Page en cours de création
+        <ImgGrid title="Décors">
+            <Image
+                src={require('../public/rendus/décors.png')}
+                alt={'mon décors 3D'}
+                className="grid-item-thumbnail"
+                placeholder="blur"
+            />
+        </ImgGrid>
+        <ImgGrid title="Personnage" delay={0.2}>
+            <Image
+                src={require('../public/rendus/RX 78-2.png')}
+                alt={'RX 78-2'}
+                className="grid-item-thumbnail"
+                placeholder="blur"
+            />
+        </ImgGrid>
+        <ImgGrid title="Collection Bottle of world" delay={0.4}>
+            <Image
+                src={require('../public/rendus/mushroom.png')}
+                alt={'bottle of mushroom'}
+                className="grid-item-thumbnail"
+                placeholder="blur"
+            />
+        </ImgGrid>
     </Layout>
 )
 
